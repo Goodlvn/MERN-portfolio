32f1cd8d949b8f869d99d58008dd7a2982a5fabe
JD Martinez <focus4ursoul@gmail.com>
2020-11-18 20:33:39 -0800

    add tagline and bio

---
+++ client/src/components/TagBio.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function TagBio() {
+    return (
+        <div style={{textAlign: "-webkit-center", width: "75%", margin: "0 auto"}}>
+            <h3>Life can be great! You just have to take a moment to take stock of your blessings.</h3>
+            <p>
+                I've been in the aquatics industry for the last five years. I started on the front lines as a lifeguard, swim instructor and front desk rep. During my time at Team Sheeper Inc. I moved up and filled a number of roles that have all taught me about life. The most important lessons remind me of the value found in community, responsibility and enjoing the work that you do. I recently stepped down from my position as head lifeguard for Rinconada Pool so I can focus on the next chapter of my career and life.
+            </p>
+        </div>
+    )
+}
